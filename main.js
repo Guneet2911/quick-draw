d65dc73a131c437f05a5956ccdd71b7ebe92d993
Guneet2911 <98748570+Guneet2911@users.noreply.github.com>
2022-11-02 17:46:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,13 +16,17 @@ function updateCanvas()
     background("white");
 }
 
-function preload() {}
+function preload() 
+{
+    classifier = ml5.imageClassifier("DoodleNet");
+}
 
 function setup()
 {
     canvas = createCanvas(280 , 280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
 }
 
 function draw() 
@@ -40,7 +44,6 @@ function check_sketch()
 {
      time_counter++;
      document.getElementById("timer").innerHTML = "Timer: " + time_counter;
-     console.log(time_counter);
 
      if(time_counter > 400)
      {
@@ -54,7 +57,34 @@ function check_sketch()
         answer_holder = "";
         updateCanvas();
      }
+
+     strokeWeight(14);
+     stroke(0);
+
+     if(mouseIsPressed)
+     {
+        line(pmouseX , pmouseY , mouseX , mouseY);
+     }
     
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas , gotResult);
+}
+
+function gotResult(error , results)
+{
+ if(error)
+ {
+    console.error(error);
+ } 
+ console.log(results);
+ drawn_sketch = results[0].label; 
+ 
+ document.getElementById("label").innerHTML = "Your sketch: " + drawn_sketch;
+ 
+ document.getElementById("confidence").innerHTML = "Confidence: " + Math.round(results[0].confidence * 100) + "%";
 }
 
 
